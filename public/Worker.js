40f20f60d73518fcf82c9747f7d7f0ad41d44bf7
gavadams <gav.adams@gmail.com>
2025-10-12 20:04:09 +0100

    Update Worker.js

--- public/Worker.js
+++ public/Worker.js
@@ -1,3 +1,25 @@
+self.onmessage = async (event) => {
+  console.log('Worker got message:', event.data);
+  const { imageBuffer, config } = event.data || {};
+  if (!imageBuffer) {
+    self.postMessage({ type: 'error', message: 'No image buffer received.' });
+    return;
+  }
+
+  for (let i = 0; i <= 100; i += 5) {
+    await new Promise((r) => setTimeout(r, 50));
+    self.postMessage({ type: 'progress', progress: i / 100 });
+  }
+
+  self.postMessage({
+    type: 'done',
+    buffer: new ArrayBuffer(0),
+    size: 256,
+    pins: config?.pins || 256,
+    lines: [],
+  });
+};
+
 w.postMessage({
   cmd: 'generate',
   imageBuffer: buf,
